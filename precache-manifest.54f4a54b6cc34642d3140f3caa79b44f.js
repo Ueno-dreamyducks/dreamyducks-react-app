3cb013965c8fe99afca7f1ed121f960b3cffaf54
takuk <taku0katy@duck.com>
2024-10-26 02:04:13 -0500

    Updates

---
+++ precache-manifest.54f4a54b6cc34642d3140f3caa79b44f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74dc7070156dd54839240e9eec8308bb",
+    "url": "/dreamyducks/index.html"
+  },
+  {
+    "revision": "4c4e8997c30f07267dca",
+    "url": "/dreamyducks/static/css/main.9bb8f643.chunk.css"
+  },
+  {
+    "revision": "2059022dac908a32d1b3",
+    "url": "/dreamyducks/static/js/2.ae4d136d.chunk.js"
+  },
+  {
+    "revision": "ba0bfa8f95d9766194b5",
+    "url": "/dreamyducks/static/js/3.7426b29a.chunk.js"
+  },
+  {
+    "revision": "4c4e8997c30f07267dca",
+    "url": "/dreamyducks/static/js/main.026707bf.chunk.js"
+  },
+  {
+    "revision": "ddfd729fe71cb96e6e38",
+    "url": "/dreamyducks/static/js/runtime~main.a303c6d6.js"
+  },
+  {
+    "revision": "24e9c71ea4d058d0e88c81be12accd31",
+    "url": "/dreamyducks/static/media/BBLogo_TransparentBackground.24e9c71e.png"
+  }
+]);
